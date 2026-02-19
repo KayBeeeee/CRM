9371c2cfeb4d3f5942cd069269a5f964d1fe451b
KayBeeeee <kabelomaponya40@gmail.com>
2026-02-19 15:28:47 +0200

    initial CRM commit

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('MYSQLUSER')}:{os.getenv('MYSQLPASSWORD')}"
+        f"@{os.getenv('MYSQLHOST')}:{os.getenv('MYSQLPORT')}/"
+        f"{os.getenv('MYSQLDATABASE')}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
