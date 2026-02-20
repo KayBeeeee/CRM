72e3cd746237292c5944c8ba139df31fd0dc2d43
KayBeeeee <kabelomaponya40@gmail.com>
2026-02-20 11:15:54 +0200

    initial CRM commit

--- models/association.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-client_contact = db.Table(
-    "client_contact",
-    db.Column("client_id", db.Integer, db.ForeignKey("client.id"), primary_key=True),
-    db.Column("contact_id", db.Integer, db.ForeignKey("contact.id"), primary_key=True)
-)
